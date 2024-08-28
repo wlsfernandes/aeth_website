eb1cc848e92884a8de8d809ba1a6883bb986d11e
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-08-28 14:16:47 -0400

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-route::get('/lang/{lang}', function ($lang) {
+Route::get('/lang/{lang}', function ($lang) {
     Session::put('locale', $lang);
     return redirect()->back(); })->name('lang.switch');
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
