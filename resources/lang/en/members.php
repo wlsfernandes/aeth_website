ce7639d09ed98982741050bd1d4e6abe0f8d20f3
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-10-02 09:49:41 -0400

    aeth members start page

---
+++ resources/lang/en/members.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'p1'=>'Join our community and have access to Hispanic experts and professionals in theological education. Ask questions, share your experience and learn from the best in the industry.',
+    'p2'=>'With this membership you have access to hours of lectures, interviews, workshops, discussions and debates with experts in the field. Until 2021, 15 Biennial Assemblies and 6 Conference Series have been held.',
+    'p3'=>'Join today for a low monthly or annual fee and gain access to exclusive discounts, access to our bookstore, resource center and much more. You`ll also have the opportunity to connect with like-minded people looking for mentors or just someone to talk theology with.', 
+    'p4'=>'Select the preferred plan and join AETH.'
+];
--- resources/views/pages/memberships.blade.php
+++ resources/views/pages/memberships.blade.php
@@ -1,7 +1,6 @@
-
 @extends('layouts.app')
 
-@section('title', 'AETH | Home') 
+@section('title', 'AETH | Members') 
 
 @section('meta-description', 'This is a brief description of the home page.')
 
@@ -11,6 +10,104 @@
 <!-- Content here -->
 
 @section('content') 
-    <p>This is your content.</p>
-    <p style="margin-top:500px;"></p>
+<section class="page-title centred">
+    <div class="bg-layer" style="background-image: url(assets/images/gallery/membership.jpg);"></div>
+    <div class="auto-container">
+        <div class="content-box">
+           
+        </div>
+    </div>
+</section>
+<section class="about-section bg-color-1 p_relative sec-pad">
+    <div class="auto-container">
+        <div class="row align-items-center clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                <div class="content_block_one">
+                    <div class="content-box p_relative">
+                        <div class="text mb_35">
+                            <!-- Image floated to the left with margin -->
+                            <figure class="image image-1" style="float: left; margin: 0 20px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member3.jpg" alt=""
+                                    style="max-width: 500px; height: auto;">
+                            </figure>
+                            <p>@lang('members.p1')</p>
+                            <p style="margin-top:20px;">@lang('members.p2')</p>
+                            <p style="margin-top:20px;">@lang('members.p3')</p>
+                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-section" style="margin-top:50px;">
+    <div class="auto-container">
+        <div class="row align-items-center clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                <div class="content_block_one">
+                    <div class="content-box p_relative">
+                        <div class="text mb_35">
+                            <!-- Image floated to the left with margin -->
+                            <figure class="image image-1" style="float: right; margin: 0 20px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member2.jpg" alt=""
+                                    style="max-width: 500px; height: auto;">
+                            </figure>
+                            <p>@lang('members.p1')</p>
+                            <p style="margin-top:20px;">@lang('members.p2')</p>
+                            <p style="margin-top:20px;">@lang('members.p3')</p>
+                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-section bg-color-1 p_relative sec-pad">
+    <div class="auto-container">
+        <div class="row align-items-center clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                <div class="content_block_one">
+                    <div class="content-box p_relative">
+                        <div class="text mb_35">
+                            <!-- Image floated to the left with margin -->
+                            <figure class="image image-1" style="float: left; margin: 0 20px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member4.jpg" alt=""
+                                    style="max-width: 500px; height: auto;">
+                            </figure>
+                            <p>@lang('members.p1')</p>
+                            <p style="margin-top:20px;">@lang('members.p2')</p>
+                            <p style="margin-top:20px;">@lang('members.p3')</p>
+                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-section" style="margin-top:50px;">
+    <div class="auto-container">
+        <div class="row align-items-center clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                <div class="content_block_one">
+                    <div class="content-box p_relative">
+                        <div class="text mb_35">
+                            <!-- Image floated to the left with margin -->
+                            <figure class="image image-1" style="float: right; margin: 0 20px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member5.jpg" alt=""
+                                    style="max-width: 500px; height: auto;">
+                            </figure>
+                            <p>@lang('members.p1')</p>
+                            <p style="margin-top:20px;">@lang('members.p2')</p>
+                            <p style="margin-top:20px;">@lang('members.p3')</p>
+                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
