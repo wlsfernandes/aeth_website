6e14574da6a41f809583a8cc9b9412c58d2a6664
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-10-02 11:19:42 -0400

    membership page

--- resources/lang/en/members.php
+++ resources/lang/en/members.php
@@ -1,8 +1,31 @@
 <?php
 
 return [
-    'p1'=>'Join our community and have access to Hispanic experts and professionals in theological education. Ask questions, share your experience and learn from the best in the industry.',
-    'p2'=>'With this membership you have access to hours of lectures, interviews, workshops, discussions and debates with experts in the field. Until 2021, 15 Biennial Assemblies and 6 Conference Series have been held.',
-    'p3'=>'Join today for a low monthly or annual fee and gain access to exclusive discounts, access to our bookstore, resource center and much more. You`ll also have the opportunity to connect with like-minded people looking for mentors or just someone to talk theology with.', 
-    'p4'=>'Select the preferred plan and join AETH.'
+    'p1' => 'Join our community and have access to Hispanic experts and professionals in theological education. Ask questions, share your experience and learn from the best in the industry.',
+    'p2' => 'With this membership you have access to hours of lectures, interviews, workshops, discussions and debates with experts in the field. Until 2021, 15 Biennial Assemblies and 6 Conference Series have been held.',
+    'p3' => 'Join today for a low monthly or annual fee and gain access to exclusive discounts, access to our bookstore, resource center and much more. You`ll also have the opportunity to connect with like-minded people looking for mentors or just someone to talk theology with.',
+    'p4' => 'Select the preferred plan and join AETH.',
+    'inst_membership' => 'Institutional Membership',
+    'benefit1' => 'Access to conferences and meetings.',
+    'benefit2' => 'Access to conference and meeting recordings for a limited period of time.',
+    'benefit3' => 'Discounted or free participation in events organized by AETH',
+    'benefit4' => '30% discount on purchases at the AETH bookstore.',
+    'benefit5' => 'Participate in the most extensive, diverse and inclusive network of experts and professionals in Hispanic theological education.',
+    'benefit6' => 'Support the maintenance and development of certification standards.',
+    'benefit7' => 'Alliances with other institutions with benefits that extend to AETH members. Examples: publishing houses, institutions of theological education.',
+    'benefit8' => 'Participation in the Network of Hispanic Bible Institutes (currently ReDET).',
+    'benefit9' => 'Opportunity to participate in quality certification processes for ministerial training programs.',
+    'ind_membership' => 'Individual Membership',
+    'benefit11' => 'Access to conferences and meetings.',
+    'benefit12' => 'Access to seminar recordings.',
+    'benefit13' => 'Access to the Justo and Catherine González Resource Center with reduced annual subscription for AETH members.',
+    'benefit14' => 'Discounted or free participation in events organized by AETH.',
+    'benefit15' => '15% discount on purchases at the AETH bookstore.',
+    'benefit16' => 'Participate in the most extensive, diverse and inclusive network of experts and professionals in Hispanic theological education.',
+    'benefit17' => 'Support the maintenance and development of certification standards.',
+    'benefit18' => 'Alliances with other institutions with benefits that extend to AETH members. Examples: publishing houses, theological education institutions.',
+    'benefit19' => 'Opportunity to become certified to participate in program quality certification processes.',
+
+    'student_membership' => 'Student Membership',
+
 ];
--- resources/views/pages/memberships.blade.php
+++ resources/views/pages/memberships.blade.php
@@ -14,7 +14,7 @@
     <div class="bg-layer" style="background-image: url(assets/images/gallery/membership.jpg);"></div>
     <div class="auto-container">
         <div class="content-box">
-           
+
         </div>
     </div>
 </section>
@@ -27,7 +27,7 @@
                         <div class="text mb_35">
                             <!-- Image floated to the left with margin -->
                             <figure class="image image-1" style="float: left; margin: 0 20px 20px 0;">
-                                <img src="assets/images/gallery/aeth-member3.jpg" alt=""
+                                <img src="assets/images/gallery/aeth-member5.jpg" alt=""
                                     style="max-width: 500px; height: auto;">
                             </figure>
                             <p>@lang('members.p1')</p>
@@ -48,15 +48,42 @@
                 <div class="content_block_one">
                     <div class="content-box p_relative">
                         <div class="text mb_35">
-                            <!-- Image floated to the left with margin -->
-                            <figure class="image image-1" style="float: right; margin: 0 20px 20px 0;">
-                                <img src="assets/images/gallery/aeth-member2.jpg" alt=""
+                            <!-- Image floated to the right with margin -->
+                            <figure class="image image-1" style="float: right; margin: 100px 50px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member3.jpg" alt=""
                                     style="max-width: 500px; height: auto;">
                             </figure>
-                            <p>@lang('members.p1')</p>
-                            <p style="margin-top:20px;">@lang('members.p2')</p>
-                            <p style="margin-top:20px;">@lang('members.p3')</p>
-                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                            <h1>@lang('members.inst_membership')</h1>
+                            <div class="row" style="margin-top:50px;">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit1')</li>
+                                        <li>@lang('members.benefit2')</li>
+                                        <li>@lang('members.benefit3')</li>
+                                        <li>@lang('members.benefit4')</li>
+                                        <li>@lang('members.benefit5')</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit6')</li>
+                                        <li>@lang('members.benefit7')</li>
+                                        <li>@lang('members.benefit8')</li>
+                                        <li>@lang('members.benefit9')</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-sm-12" style="margin:50px;">
+                                <p>Select the payment frequency for the type of membership you want:</p>
+                                <div style="margin:50px;">
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;"><span>$20 Montlhy</span></button>
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;margin-left:120px;"><span>$200
+                                            Yearly</span></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -64,7 +91,59 @@
         </div>
     </div>
 </section>
+
+
 <section class="about-section bg-color-1 p_relative sec-pad">
+    <div class="auto-container">
+        <div class="row align-items-center clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                <div class="content_block_one">
+                    <div class="content-box p_relative">
+                        <div class="text mb_35">
+                            <!-- Image floated to the left with margin -->
+                            <figure class="image image-1" style="float: right; margin: 0 20px 20px 0;">
+                                <img src="assets/images/gallery/aeth-member6.jpg" alt=""
+                                    style="max-width: 500px; height: auto;">
+                            </figure>
+                            <h1>@lang('members.ind_membership')</h1>
+                            <div class="row" style="margin-top:50px;">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit11')</li>
+                                        <li>@lang('members.benefit12')</li>
+                                        <li>@lang('members.benefit13')</li>
+                                        <li>@lang('members.benefit14')</li>
+                                        <li>@lang('members.benefit15')</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit16')</li>
+                                        <li>@lang('members.benefit17')</li>
+                                        <li>@lang('members.benefit18')</li>
+                                        <li>@lang('members.benefit19')</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-sm-12" style="margin:50px;">
+                                <p>Select the payment frequency for the type of membership you want:</p>
+                                <div style="margin:50px;">
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;"><span>$10 Montlhy</span></button>
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;margin-left:120px;"><span>$100
+                                            Yearly</span></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about-section" style="margin-top:50px;">
     <div class="auto-container">
         <div class="row align-items-center clearfix">
             <div class="col-lg-12 col-md-12 col-sm-12 content-column">
@@ -76,33 +155,37 @@
                                 <img src="assets/images/gallery/aeth-member4.jpg" alt=""
                                     style="max-width: 500px; height: auto;">
                             </figure>
-                            <p>@lang('members.p1')</p>
-                            <p style="margin-top:20px;">@lang('members.p2')</p>
-                            <p style="margin-top:20px;">@lang('members.p3')</p>
-                            <p style="margin-top:20px;">@lang('members.p4')</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="about-section" style="margin-top:50px;">
-    <div class="auto-container">
-        <div class="row align-items-center clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 content-column">
-                <div class="content_block_one">
-                    <div class="content-box p_relative">
-                        <div class="text mb_35">
-                            <!-- Image floated to the left with margin -->
-                            <figure class="image image-1" style="float: right; margin: 0 20px 20px 0;">
-                                <img src="assets/images/gallery/aeth-member5.jpg" alt=""
-                                    style="max-width: 500px; height: auto;">
-                            </figure>
-                            <p>@lang('members.p1')</p>
-                            <p style="margin-top:20px;">@lang('members.p2')</p>
-                            <p style="margin-top:20px;">@lang('members.p3')</p>
-                            <p style="margin-top:20px;">@lang('members.p4')</p>
+                            <h1>@lang('members.student_membership')</h1>
+                            <div class="row" style="margin-top:50px;">
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit11')</li>
+                                        <li>@lang('members.benefit12')</li>
+                                        <li>@lang('members.benefit13')</li>
+                                        <li>@lang('members.benefit14')</li>
+                                        <li>@lang('members.benefit15')</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <ul class="list-style-one clearfix">
+                                        <li>@lang('members.benefit16')</li>
+                                        <li>@lang('members.benefit17')</li>
+                                        <li>@lang('members.benefit18')</li>
+                                        <li>@lang('members.benefit19')</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-md-6 col-sm-12" style="margin:50px;">
+                                <p>Select the payment frequency for the type of membership you want:</p>
+                                <div style="margin:50px;">
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;"><span>$10 Montlhy</span></button>
+                                    <button class="donate-box-btn theme-btn-one"
+                                        style="padding:12px 1px 12px 12px;margin-left:120px;"><span>$100
+                                            Yearly</span></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
